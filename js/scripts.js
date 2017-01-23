b00368a52ca52f5c23b260d8f8eeed842fcd17d6
Dylan Stackhouse and Jennifer Beem <jennifer@>
2017-01-23 09:36:19 -0800

    take user input and display entries in a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // business logic
-function Places(name, landmarks, location, dates, description) {
+function Place(name, location, landmarks, dates, description) {
   this.name = name;
   this.landmarks = landmarks;
   this.location = location;
@@ -7,10 +7,27 @@ function Places(name, landmarks, location, dates, description) {
   this.description = description;
 }
 
-
-
-
-
-
+Place.prototype.createEntry = function() {
+  return "<li><ul><li>Place: " + this.name +"</li><li>Location: " + this.location + "</li> <li>Landmarks: " + this.landmarks + "</li><li>Ideal dates: " + this.dates + "</li><li>Description: " + this.description + "</li></ul></li>"
+}
 
 //user interface logic
+$(document).ready(function() {
+  $("form#places").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("input#name").val();
+    var inputtedLocation = $("input#location").val();
+    var inputtedLandmarks = $("input#landmarks").val().split(",");
+    var inputtedDates = $("input#dates").val();
+    var inputtedDescription = $("textarea#description").val();
+
+    var newPlace = new Place(inputtedName, inputtedLocation, inputtedLandmarks, inputtedDates, inputtedDescription);
+
+
+    $(".entries").show();
+    $("ul#entries").append(newPlace.createEntry());
+
+    $(".input").val("");
+  })
+})
