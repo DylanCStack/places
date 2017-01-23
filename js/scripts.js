0d98801509c443978d10d86d79df23bf306e8576
Dylan Stackhouse and Jennifer Beem <jennifer@>
2017-01-23 08:59:18 -0800

    add ability to create place object using constructor

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// business logic
+function Places(name, landmarks, location, dates, description) {
+  this.name = name;
+  this.landmarks = landmarks;
+  this.location = location;
+  this.dates = dates;
+  this.description = description;
+}
+
+
+
+
+
+
+
+//user interface logic
